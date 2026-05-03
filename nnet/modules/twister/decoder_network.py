d9d0a352a48c2626b78738f0ca6ece97a788805b
ssrhaso <hasaana2005@gmail.com>
2026-05-03 12:45:54 +0100

    CHORE: Drop trailing modifier comment from decoder_network.py copyright

--- nnet/modules/twister/decoder_network.py
+++ nnet/modules/twister/decoder_network.py
@@ -1,5 +1,5 @@
 # Copyright 2025, Maxime Burchi.
-# Copyright 2025, Hasaan Ahmad.  # ECHELON modifications
+# Copyright 2025, Hasaan Ahmad.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
