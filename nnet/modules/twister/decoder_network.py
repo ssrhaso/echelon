8ebda8a2c617ee53da659981dd57987c2a548e8f
ssrhaso <hasaana2005@gmail.com>
2026-05-04 09:00:22 +0100

    CHORE: Normalize modification notice on decoder_network.py

--- nnet/modules/twister/decoder_network.py
+++ nnet/modules/twister/decoder_network.py
@@ -1,5 +1,5 @@
 # Copyright 2025, Maxime Burchi.
-# Copyright 2025, Hasaan Ahmad.
+# Modifications Copyright 2025-2026, Hasaan Ahmad — adapted for ECHELON.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
